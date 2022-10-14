d56db00df802090c48b90d96c7aee7db502d50ba
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-10-14 01:59:05 -0500

    PATCH added

--- server/src/controllers/employees.controller.js
+++ server/src/controllers/employees.controller.js
@@ -25,7 +25,23 @@ export const createEmployee = async (req, res) => {
   })
 }
 
-export const updateEmployee = (req, res) => res.send("update employees")
+export const updateEmployee = async (req, res) => {
+  const { id } = req.params
+  const { name, salary } = req.body
+  console.log(id, name, salary);
+
+  const [result] = await pool.query('UPDATE employee SET name = IFNULL(?,name), salary = IFNULL(?,salary) WHERE idEmployee = ?', [name, salary, id])
+
+  if (result.affectedRows === 0) return res.status(404).json({
+    message: "Employee not found"
+  })
+
+  const [rows] = await pool.query('SELECT * FROM employee WHERE idEmployee = ?', [id])
+
+  // console.log(result);
+  res.json(rows[0])
+
+}
 
 export const deleteEmployee = async (req, res) => {
   const [result] = await pool.query('DELETE FROM employee WHERE idEmployee=?;', [req.params.id])
