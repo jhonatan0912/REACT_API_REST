0ee3d144f5fc69cf364adcfcbac2abf82c9505d4
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-10-14 01:16:05 -0500

    some advance API REST BACKEND

---
+++ server/db/database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS companydb;
+
+INSERT INTO `companydb`.`employee`
+(`idEmployee`,
+`name`,
+`salary`)
+VALUES
+(1,
+'Joe',
+1000),
+(2,
+'Henry',
+2000),
+(3,
+'Sam',
+2500),
+(4,
+'Max',
+1500);
